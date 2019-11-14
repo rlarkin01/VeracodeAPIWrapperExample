b1bd756e8adf0f74be0a901d6a89a18dd98f8538
rlarkin01 <rlarkin@veracode.com>
2019-11-14 10:56:45 -0500

    Made credentials origin a little more explicit.

--- src/main/java/hello/Veracode.java
+++ src/main/java/hello/Veracode.java
@@ -12,8 +12,6 @@ import javax.xml.xpath.*;
 import java.io.IOException;
 import java.io.StringReader;
 
-import static hello.Credentials.*;
-
 class Veracode {
 
     static String retrieveResults(String appName, String Sandbox) throws ParserConfigurationException, SAXException, XPathExpressionException, IOException {
@@ -21,7 +19,7 @@ class Veracode {
         String buildId = getBuildId(appName);
 
         ResultsAPIWrapper wrapper = new ResultsAPIWrapper();
-        wrapper.setUpApiCredentials(API_ID, API_KEY);
+        wrapper.setUpApiCredentials(Credentials.API_ID, Credentials.API_KEY);
         String xml = wrapper.detailedReport(buildId);
 
         return xml;
@@ -32,7 +30,7 @@ class Veracode {
         String buildId = getBuildId(appName);
 
         ResultsAPIWrapper wrapper = new ResultsAPIWrapper();
-        wrapper.setUpApiCredentials(API_ID, API_KEY);
+        wrapper.setUpApiCredentials(Credentials.API_ID, Credentials.API_KEY);
         byte[] pdf = wrapper.detailedReportPdf(buildId);
 
         return pdf;
@@ -40,7 +38,7 @@ class Veracode {
 
     static String getBuildId(String appName) throws ParserConfigurationException, SAXException, XPathExpressionException, IOException {
         ResultsAPIWrapper wrapper = new ResultsAPIWrapper();
-        wrapper.setUpApiCredentials(API_ID, API_KEY);
+        wrapper.setUpApiCredentials(Credentials.API_ID, Credentials.API_KEY);
         String xml = wrapper.getAppBuilds();
 
         return findInXMLString(xml, String.format("/applicationbuilds/application[contains(@app_name,'%s')]/build/@build_id", appName));
@@ -61,7 +59,7 @@ class Veracode {
 
     private static String beginScan(String appId) throws IOException, XPathExpressionException, SAXException, ParserConfigurationException {
         UploadAPIWrapper wrapper = new UploadAPIWrapper();
-        wrapper.setUpApiCredentials(API_ID, API_KEY);
+        wrapper.setUpApiCredentials(Credentials.API_ID, Credentials.API_KEY);
 //        String xml = wrapper.beginPreScan(appId, null, "auto_scan", "scan_all_nonfatal_top_level_modules");
         String xml = wrapper.beginPreScan(appId, null, "auto_scan");
 
@@ -70,7 +68,7 @@ class Veracode {
 
     private static String uploadApplication(String appId, String file) throws IOException, XPathExpressionException, SAXException, ParserConfigurationException {
         UploadAPIWrapper wrapper = new UploadAPIWrapper();
-        wrapper.setUpApiCredentials(API_ID, API_KEY);
+        wrapper.setUpApiCredentials(Credentials.API_ID, Credentials.API_KEY);
         String xml = wrapper.uploadFile(appId, file);
 
         return findInXMLString(xml, "/filelist/file/@file_status");
@@ -78,7 +76,7 @@ class Veracode {
 
     private static String getApplicationId(String appName) throws IOException, XPathExpressionException, ParserConfigurationException, SAXException {
         UploadAPIWrapper wrapper = new UploadAPIWrapper();
-        wrapper.setUpApiCredentials(API_ID, API_KEY);
+        wrapper.setUpApiCredentials(Credentials.API_ID, Credentials.API_KEY);
         String xml = wrapper.getAppList();
 
         String appId = findInXMLString(xml, String.format("/applist/app[contains(@app_name,'%s')]/@app_id", appName));
